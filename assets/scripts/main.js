10bb91763c6588172c434a080263b380881be5db
Arthur Aguilar <arthurg.programming@gmail.com>
2024-02-22 13:17:30 -0300

    add:Animações

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,16 +2,9 @@
 const elements = document.querySelectorAll('.hidden');
 
 const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show')
-            entry.target.classList.remove('hidden')
-            // console.log('ok')
-        } else {
-            entry.target.classList.add('hidden')
-            entry.target.classList.remove('show')
-            // console.log('n')
-        }
+    entries.forEach((entry) => {        
+        entry.target.classList.add('show')
+        entry.target.classList.remove('hidden')
     });
 });
 
