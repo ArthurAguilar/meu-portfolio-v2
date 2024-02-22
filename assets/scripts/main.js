9f8b65fdad4f65d40f7c9019ac9411c2ca065c96
Arthur Aguilar <arthurg.programming@gmail.com>
2024-02-22 13:33:27 -0300

    ajeitando animação

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,15 @@
-// alert("Em construção!");
+alert("Em construção!");
 const elements = document.querySelectorAll('.hidden');
 
-const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {        
-        entry.target.classList.add('show')
-        entry.target.classList.remove('hidden')
-    });
-});
+const myObserver = new IntersectionObserver(
+    entries => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show')
+                entry.target.classList.remove('hidden')
+            }
+        })
+})
 
 elements.forEach((section) => {
     myObserver.observe(section)
